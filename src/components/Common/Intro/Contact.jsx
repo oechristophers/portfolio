90f7b8eecf021d7b0e49443831de3fdcec0ff99e
oechristophers <oechristophers2020@gmail.com>
2025-01-24 08:25:06 +0100

    Deployment fixes -4

--- src/components/Common/Intro/Contact.jsx
+++ src/components/Common/Intro/Contact.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
-import { CONTACTS } from "@/constants/constants";
-import animationData from "@/data/confetti.json";
-import Lottie from "lottie-react";
+import dynamic from 'next/dynamic';
+
+const LottieNoSSR = dynamic(() => import('lottie-react'), { ssr: false });
+
+import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
-import MagicButton from "@/components/ui/MagicButton";
 import { BackgroundGradientAnimation } from "@/components/ui/background-gradient";
+import animationData from "@/data/confetti.json";
 
 const Contact = () => {
   const [copied, setCopied] = useState(false);
@@ -14,33 +15,29 @@ const Contact = () => {
       .writeText("oecristophers2023@gmail.com")
       .then(() => {
         setCopied(true);
-  
         // Reset `copied` to false after 5 seconds
         setTimeout(() => setCopied(false), 5000);
       })
       .catch(() => console.error("Failed to copy email."));
   };
-  
+
   return (
-    <div className="relative flex justify-center  w-full items-center h-40">
-    <BackgroundGradientAnimation></BackgroundGradientAnimation>
-          <div className="absolute top-5  ">
-            <Lottie
-              animationData={animationData}
-              loop={copied} // Plays only if copied
-              autoplay={copied} // Starts playing when copied
-            />
-          </div>
-          {/* <MagicButton
-            title={copied ? "Email is Copied!" : "Copy my email "}
-            icon={<IoCopyOutline />}
-            position="left"
-            handleClick={handleCopy}
-            otherClasses=""
-          /> */}
-          <button className="px-3 rounded-full z-[100] flex  text-white border justify-center items-center gap-1 bg-[#00000089] text-[.9rem] py-2" onClick={handleCopy}>
-          <IoCopyOutline />  {copied ? "Email is Copied!" : "Copy my email "}
-          </button>
+    <div className="relative flex justify-center w-full items-center h-40">
+      <BackgroundGradientAnimation />
+      <div className="absolute top-5">
+        <LottieNoSSR
+          animationData={animationData}
+          loop={copied} // Plays only if copied
+          autoplay={copied} // Starts playing when copied
+        />
+      </div>
+
+      <button
+        className="px-3 rounded-full z-[100] flex text-white border justify-center items-center gap-1 bg-[#00000089] text-[.9rem] py-2"
+        onClick={handleCopy}
+      >
+        <IoCopyOutline /> {copied ? "Email is Copied!" : "Copy my email "}
+      </button>
     </div>
   );
 };
